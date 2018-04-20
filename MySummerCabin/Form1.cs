f063261e8a23abbb5e9b05b7a9372cf867896547
Jere Kähäri <Jere.Kahari@edu.savonia.fi>
2018-04-20 12:54:03 +0300

    l

--- MySummerCabin/Form1.cs
+++ MySummerCabin/Form1.cs
@@ -14,7 +14,7 @@ namespace MySummerCabin
     {
         public Form1()
         {
-            //Tästä se lähtee!!!
+            //Tästä se lähtee!!!!
             InitializeComponent();
         }
     }
